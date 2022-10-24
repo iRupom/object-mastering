24c29d29f4594d3b529155830bd07b6f971e5755
rrakib2 <rakib.rupom007@gmail.com>
2022-10-24 16:01:35 +0600

    timeout practice

---
+++ timeout.js
@@ -0,0 +1,20 @@
+console.log('My name is Rakib');
+console.log('I love my family');
+console.log('I want to live with my family');
+
+setTimeout(() => {
+    console.log('This is the end of my story');
+}, 400);
+setTimeout(() => {
+    console.log("I am happy with my family.")
+}, 500);
+
+setTimeout(() => {
+    console.log('I am not happy');
+}, 200);
+
+setInterval(() => {
+    console.log('I want to become good software engineer');
+}, 100);
+
+console.log('My hometown is sirajganj');
